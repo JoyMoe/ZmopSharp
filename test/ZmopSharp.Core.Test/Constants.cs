158c032f55b9b46f39a2df10e76cfac6d0e5e62e
Chino Chang <chino@joymoe.com>
2017-11-07 12:52:29 +0800

    update ZmopSharp.Core.Test

---
+++ test/ZmopSharp.Core.Test/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace ZmopSharp.Core.Test
+{
+    public static class Constants
+    {
+        public static class Encryption
+        {
+            public const string PrivateKey = @"MIICXAIBAAKBgQDRNHK6ljEQmuVjnNGYO3DXsIJdi4QYJ4y5fgGh7D/Mbh2e463Ak35uNkIktHH+NNGAK3UQg3MWOPU3UEUOWZlD+inRHBI4ZVrAMaFT9JlxuAblxZbbXnl4pJsZHgYVPyQrn9L7B8o+Sovs8IzIgK9fklHl880oyQ6/uxrEoQHaGwIDAQABAoGATixEBrbipKrMtpk9tJ3Zf69kN2JPvNriedm88G2gNVqcVOgM4B39K1+4/ZAz3GKOOk+C0eXcllw4PTAQbdlLLG+RgQwd8Q6+Iz+S1jAQZxVUpRUF630TTgfqHSgQnDBe7vhS/hFHDkxMHk4yBpWSaLdjJ1+lq8qirIb+Gu3woeECQQDqgJxjXrMdcPZPhef67WDkzIU1fcPW9+dm178LdQ3zFVF0i7sSsv4L1oTn89JP2AJReMGSFpejTo9q4PIWTBqLAkEA5GIlWRzsViYrSye6bl7qg+MzH838xJl79zsLsGNX4nmB6py/D47oe5DH5qP4wZKLc0HWkQ6v1hQT515xRRqAsQJAdY6f15jjxgCZGeiRENPrXQfCR7ySbrO8R2wkk8PMiG3JddmOoKMmhBy4Z+dtKSWqk9krgObegbmiX/wAoHjqXQJBAKdIzJqNeP9RTfzqyL/3fkfEr7noI/HNiuP4uAMngUQ+RE8ZKUm4H8oCrqneCWOgd8yguQ3feCSS6FFc+BLp6sECQFGT4iZGvdYa2JfDVM2fhIylQmHPuaFROfRZziE/55Gce4DiFgSSCe9mkGyLxyi1/uarK7GtAu0/V5uhw3GGPfg=";
+
+            public const string PublicKey = @"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDRNHK6ljEQmuVjnNGYO3DXsIJdi4QYJ4y5fgGh7D/Mbh2e463Ak35uNkIktHH+NNGAK3UQg3MWOPU3UEUOWZlD+inRHBI4ZVrAMaFT9JlxuAblxZbbXnl4pJsZHgYVPyQrn9L7B8o+Sovs8IzIgK9fklHl880oyQ6/uxrEoQHaGwIDAQAB";
+        }
+    }
+}
--- test/ZmopSharp.Core.Test/EncryptionTests.cs
+++ test/ZmopSharp.Core.Test/EncryptionTests.cs
@@ -1,40 +1,24 @@
-using System;
 using Xunit;
 
 namespace ZmopSharp.Core.Test
 {
     public class EncryptionTests
     {
-        private static readonly string _privateKey = @"MIICXAIBAAKBgQDRNHK6ljEQmuVjnNGYO3DXsIJdi4QYJ4y5fgGh7D/Mbh2e463A
-k35uNkIktHH+NNGAK3UQg3MWOPU3UEUOWZlD+inRHBI4ZVrAMaFT9JlxuAblxZbb
-Xnl4pJsZHgYVPyQrn9L7B8o+Sovs8IzIgK9fklHl880oyQ6/uxrEoQHaGwIDAQAB
-AoGATixEBrbipKrMtpk9tJ3Zf69kN2JPvNriedm88G2gNVqcVOgM4B39K1+4/ZAz
-3GKOOk+C0eXcllw4PTAQbdlLLG+RgQwd8Q6+Iz+S1jAQZxVUpRUF630TTgfqHSgQ
-nDBe7vhS/hFHDkxMHk4yBpWSaLdjJ1+lq8qirIb+Gu3woeECQQDqgJxjXrMdcPZP
-hef67WDkzIU1fcPW9+dm178LdQ3zFVF0i7sSsv4L1oTn89JP2AJReMGSFpejTo9q
-4PIWTBqLAkEA5GIlWRzsViYrSye6bl7qg+MzH838xJl79zsLsGNX4nmB6py/D47o
-e5DH5qP4wZKLc0HWkQ6v1hQT515xRRqAsQJAdY6f15jjxgCZGeiRENPrXQfCR7yS
-brO8R2wkk8PMiG3JddmOoKMmhBy4Z+dtKSWqk9krgObegbmiX/wAoHjqXQJBAKdI
-zJqNeP9RTfzqyL/3fkfEr7noI/HNiuP4uAMngUQ+RE8ZKUm4H8oCrqneCWOgd8yg
-uQ3feCSS6FFc+BLp6sECQFGT4iZGvdYa2JfDVM2fhIylQmHPuaFROfRZziE/55Gc
-e4DiFgSSCe9mkGyLxyi1/uarK7GtAu0/V5uhw3GGPfg=".Replace("\n", "");
-        
-        private static readonly string _publicKey = @"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDRNHK6ljEQmuVjnNGYO3DXsIJd
-i4QYJ4y5fgGh7D/Mbh2e463Ak35uNkIktHH+NNGAK3UQg3MWOPU3UEUOWZlD+inR
-HBI4ZVrAMaFT9JlxuAblxZbbXnl4pJsZHgYVPyQrn9L7B8o+Sovs8IzIgK9fklHl
-880oyQ6/uxrEoQHaGwIDAQAB".Replace("\n", "");
-        
+        private const string PrivateKey = Constants.Encryption.PrivateKey;
+
+        private const string PublicKey = Constants.Encryption.PublicKey;
+
         [Fact]
         public void Can_Encrypt_And_Decrypt()
         {
             const string plainText = "ZmopSharp";
 
-            var cipher = Encryption.Encrypt(_publicKey, plainText);
+            var cipher = Encryption.Encrypt(PublicKey, plainText);
             Assert.NotEmpty(cipher);
-            
-            var plain = Encryption.Decrypt(_privateKey, cipher);
+
+            var plain = Encryption.Decrypt(PrivateKey, cipher);
             Assert.NotEmpty(plain);
-            
+
             Assert.Equal(plain, plainText);
         }
 
@@ -43,10 +27,10 @@ HBI4ZVrAMaFT9JlxuAblxZbbXnl4pJsZHgYVPyQrn9L7B8o+Sovs8IzIgK9fklHl
         {
             const string plainText = "ZmopSharp";
 
-            var signature = Encryption.Sign(_privateKey, plainText);
+            var signature = Encryption.Sign(PrivateKey, plainText);
             Assert.NotEmpty(signature);
-            
-            var passed = Encryption.Verify(_publicKey, plainText, signature);
+
+            var passed = Encryption.Verify(PublicKey, plainText, signature);
             Assert.True(passed);
         }
     }
